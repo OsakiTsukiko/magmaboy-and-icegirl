b7a6ab5feb60bff515b941e763a579210c5e51ab
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2023-01-16 18:11:37 +0200

    add [player + test scene]

--- src/entity/entity.gd
+++ src/entity/entity.gd
@@ -1,12 +1,14 @@
 class_name Entity2D
 extends KinematicBody2D
 
-const SPEED = Vector2(150.0, 350.0)
-const GRAVITY_VEC = Vector2(0.0, 98.0)
-const FLOOR_NORMAL = Vector2.UP
+const SPEED := Vector2(150.0, 350.0)
+const GRAVITY_VEC := Vector2(0.0, 980.0)
+const FLOOR_NORMAL := Vector2.UP
+const SLOPE_SLIDE_STOP := true
+const FLOOR_DETECT_DISTANCE = 20.0
 
 var linear_vel = Vector2.ZERO
 
 func _physics_process(delta):
 	linear_vel += GRAVITY_VEC * delta
-
+#	linear_vel = move_and_slide(linear_vel, FLOOR_NORMAL, SLOPE_SLIDE_STOP)
---
+++ src/player/Player.gd
@@ -0,0 +1,69 @@
+extends Entity2D
+
+onready var sprite = $Sprite
+onready var ray_cast = $RayCast2D
+
+puppet var character: int = 0
+
+puppet func select_character(character: int):
+	pass
+
+puppet func update_location(pos: Vector2, vel: Vector2):
+	position = pos
+	linear_vel = vel
+
+func _physics_process(delta):
+#	if (is_network_master()):
+		# ^ weird method, should look more into it
+	if (true):
+		var direction = get_direction()
+		var is_jump_interrupted = Input.is_action_just_released("jump") && linear_vel.y < 0.0
+		linear_vel = calculate_move_velocity(linear_vel, direction, SPEED, is_jump_interrupted)
+		
+		var snap_vector = Vector2.ZERO
+		if direction.y == 0.0:
+			snap_vector = Vector2.DOWN * FLOOR_DETECT_DISTANCE
+		var is_on_platform = ray_cast.is_colliding()
+		linear_vel = move_and_slide_with_snap(
+			linear_vel,
+			snap_vector,
+			FLOOR_NORMAL,
+			!is_on_platform,
+			4,
+			0.9,
+			false
+		)
+		
+		if direction.x != 0:
+			if direction.x > 0:
+				if ( sprite.scale.x < 0 ): sprite.scale.x *= -1
+			else:
+				if ( sprite.scale.x >= 0 ): sprite.scale.x *= -1
+		
+		# TODO: ANIMATIONS
+		
+#		rpc("set_pos_and_motion",position,linear_vel)
+
+# UTILS
+
+func get_direction():
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1 if is_on_floor() and Input.is_action_just_pressed("jump") else 0
+	)
+
+func calculate_move_velocity (
+		linear_velocity,
+		direction,
+		speed,
+		is_jump_interrupted
+	):
+	var velocity = linear_velocity
+	velocity.x = speed.x * direction.x
+	if direction.y != 0.0:
+		velocity.y = speed.y * direction.y
+	if is_jump_interrupted:
+		# Decrease the Y velocity by multiplying it, but don't set it to 0
+		# as to not be too abrupt.
+		velocity.y *= 0.6
+	return velocity
