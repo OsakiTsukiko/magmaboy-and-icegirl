daaaf373bbdd59082c9f7c153571c35444a192ab
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2023-01-21 17:13:07 +0200

    changes to host menu scene

--- src/ui/host_menu/HostMenu.gd
+++ src/ui/host_menu/HostMenu.gd
@@ -1,8 +1,13 @@
 extends Control
 
+var main_menu_scene = load("res://src/ui/main_menu/MainMenu.tscn")
+
 onready var CharacterSelectorINP = $BG/CenterContainer/VBoxContainer/CharacterSelector
 
 func _ready():
 	CharacterSelectorINP.add_item("Magma Boy")
 	CharacterSelectorINP.add_item("Ice Girl")
 	pass
+
+func _on_BackBTN_pressed():
+	get_tree().change_scene_to(main_menu_scene)
--- src/ui/main_menu/MainMenu.gd
+++ src/ui/main_menu/MainMenu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+var host_menu_scene = load("res://src/ui/host_menu/HostMenu.tscn")
+
 func _on_GithubBTN_pressed():
 	if (ProjectSettings.has_setting("info/github_url")):
 		OS.shell_open(ProjectSettings.get("info/github_url"))
+
+func _on_HostBTN_pressed():
+	get_tree().change_scene_to(host_menu_scene)
